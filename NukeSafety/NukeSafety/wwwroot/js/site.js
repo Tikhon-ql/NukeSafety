5563c0cf9e749d6ae73caa9d7ceb6bd7f0d3c5fa
Tikhon-ql <tikhon.grek@mail.ru>
2022-10-31 01:38:02 +0300

    Explosion UI implementation

--- NukeSafety/NukeSafety/wwwroot/js/site.js
+++ NukeSafety/NukeSafety/wwwroot/js/site.js
@@ -1,25 +1,78 @@
-﻿
-ymaps.ready(init);
-
+﻿let coords;
 let map;
 
+let longitude = document.getElementById("longitude");
+let latitude = document.getElementById("latitude");
+let buttonDetonate = document.getElementById("buttonDetonate");
+//------------------
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+function success(event) {
+    coords = [event.coords.latitude, event.coords.longitude];
+    console.dir(coords);
+}
+function error(event) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+//------------------
+
+ymaps.ready(init);
+
 function init() {
-    map = new ymaps.Map('map-test', {
-        center: [55.76, 37.60],
-        zoom: 15
-    });
 
-    map.events.add('click', function (e) {
-        console.log(e.get('coords'));
-        var myCircle = new ymaps.Circle([
+    longitude.textContent = coords[0];
+    latitude.textContent = coords[1];
 
-           e.get('coords'),
-            10000
+    var myMap = new YandexMap(coords);
+
+    console.dir(coords);
+  
+
+    buttonDetonate.addEventListener('click', function (e) {
+        e.preventDefault();
+        //alert("anime");
+
+        //console.dir(map.geoObjects);
+        myMap.detonate();
+    })
+}
+//----------------------------
+
+
+class YandexMap
+{
+    constructor(coords){
+        this.map = new ymaps.Map('map-test', {
+            center: coords,
+            zoom: 15
+        });
+
+        this.map.events.add('click', function(e){
+            //document.getElementById("longitude").textContent = e.get('coords')[0];
+            //document.getElementById("latitude").textContent = e.get('coords')[1];
+            longitude.textContent = e.get('coords')[0];
+            latitude.textContent = e.get('coords')[1];
+        })
+    }
+    detonate() {
+        //console.dir(map);
+        console.dir(this);
+        var blasteYield = document.forms[0][0].value;
+        var myCircle1 = new ymaps.Circle([
+            [longitude.textContent, latitude.textContent],
+            blasteYield
         ], {
             balloonContent: "Радиус круга - 10 км",
             hintContent: "Подвинь меня"
         }, {
-            draggable: true,
+            draggable: false,
             fillColor: "#DB709377",
             strokeColor: "#990066",
             strokeOpacity: 0.8,
@@ -27,41 +80,68 @@ function init() {
         });
         var myCircle2 = new ymaps.Circle([
 
-            e.get('coords'),
-            20000
+            [longitude.textContent, latitude.textContent],
+            Number(blasteYield) + Number(500)
         ], {
             balloonContent: "Радиус круга - 10 км",
             hintContent: "Подвинь меня"
         }, {
-            draggable: true,
+            draggable: false,
             fillColor: "#235789",
             fillOpacity: 0.5,
             strokeColor: "#ED1C24",
             strokeOpacity: 0.8,
             strokeWidth: 10
         });
-        map.geoObjects.add(myCircle);
-        map.geoObjects.add(myCircle2);
-    })
+        var myCircle3 = new ymaps.Circle([
+
+            [longitude.textContent, latitude.textContent],
+            Number(blasteYield) + Number(1500)
+        ], {
+            balloonContent: "Радиус круга - 10 км",
+            hintContent: "Подвинь меня"
+        }, {
+            draggable: false,
+            fillColor: "#8332AC",
+            fillOpacity: 0.5,
+            strokeColor: "#462749",
+            strokeOpacity: 0.8,
+            strokeWidth: 10
+        });
+        var myCircle4 = new ymaps.Circle([
+
+            [longitude.textContent, latitude.textContent],
+            Number(blasteYield) + Number(2500)
+        ], {
+            balloonContent: "Радиус круга - 10 км",
+            hintContent: "Подвинь меня"
+        }, {
+            draggable: false,
+            fillColor: "#BAD1CD",
+            fillOpacity: 0.5,
+            strokeColor: "#F2D1C9",
+            strokeOpacity: 0.8,
+            strokeWidth: 10
+        });
+        var myCircle5 = new ymaps.Circle([
+
+            [longitude.textContent, latitude.textContent],
+            Number(blasteYield) + Number(4000)
+        ], {
+            balloonContent: "Радиус круга - 10 км",
+            hintContent: "Подвинь меня"
+        }, {
+            draggable: false,
+            fillColor: "#51E5FF",
+            fillOpacity: 0.5,
+            strokeColor: "#EC368D",
+            strokeOpacity: 0.8,
+            strokeWidth: 10
+        });
+        this.map.geoObjects.add(myCircle1);
+        this.map.geoObjects.add(myCircle2);
+        this.map.geoObjects.add(myCircle3);
+        this.map.geoObjects.add(myCircle4);
+        this.map.geoObjects.add(myCircle5);
+    }
 }
-
-
-
-
-//const options = {
-//    enableHighAccuracy: true,
-//    timeout: 5000,
-//    maximumAge: 0
-//}
-
-
-
-//function success(pos) {
-
-//}
-//function error(err) {
-//    console.warn(`ERROR(${err.code}): ${err.message}`);
-//}
-
-
-//navigator.geolocation.getCurrentPosition(success, error, options);
